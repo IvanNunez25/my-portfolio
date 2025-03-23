9fda9420e15ec04e4b2e8c1e516e1d40a9354629
Ivanovicx Nuñez <ivan25606@gmail.com>
2025-03-23 14:39:45 -0600

    Segmented Control as Menu

---
+++ src/components/SegmentedControl.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { HomeIcon, DocumentDuplicateIcon, NewspaperIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+const links = [
+    { name: 'Home', href: '/', icon: HomeIcon },
+    { name: 'Proyectos', href: '/projects', icon: DocumentDuplicateIcon },
+    { name: 'Blog', href: '/blog', icon: NewspaperIcon },
+];
+
+export default function SegmentedControl() {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex w-full max-w-xs sm:max-w-md mx-auto rounded-2xl p-1" style={{ backgroundColor: 'var(--segmented-bg)' }}>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              "flex-2 text-center py-2 rounded-2xl text-sm font-medium transition-colors duration-200",
+              pathname === link.href
+                ? "shadow" // Aplica sombra si está activo
+                : "hover:bg-gray-300",
+            )}
+            style={{
+              color: pathname === link.href ? "var(--segmented-active-text)" : "var(--segmented-text)",
+              backgroundColor: pathname === link.href ? "var(--segmented-active-bg)" : "transparent",
+            }}
+          >
+            <LinkIcon className="w-5 mx-auto" />
+            <p>{link.name}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/projects/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <p className="w-full h-9 text-2xl text-black">Proyectos</p>
+    )
+}
